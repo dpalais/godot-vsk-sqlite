9a6adfa793a2964fd0dcaa85b8993cb23b0c7546
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-17 20:49:09 -0700

    Revert "Merge remote-tracking branch 'origin/torrent-next'"
    This reverts commit b45b451b301e660fc4377a1dc69e040414e4f571.

--- thirdparty/torrent/openssl/openssl.h
+++
@@ -1,1 +0,0 @@
-#include "thirdparty/mbedtls/include/mbedtls/compat-1.3.h"
--- thirdparty/torrent/sqltorrent.h
+++
@@ -1,8 +0,0 @@
-#ifndef SQLTORRENT_H_INCLUDED
-#define SQLTORRENT_H_INCLUDED
-
-#if defined(_WIN32) && !defined(SQLITE_CORE)
-__declspec(dllimport)
-#endif
-int sqltorrent_init(int make_default);
-#endif
