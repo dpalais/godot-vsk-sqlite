ac294d5f0469b2c4225059fb25ec31ad3e5b98b7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-02-15 21:14:09 -0800

    Do not build msvc.

--- config.py
+++ config.py
@@ -14,6 +14,11 @@ def can_build(env, platform):
         if platform == "windows":
             print("Use `scoop install rustup-gnu` to install rust.")
         return False
+    if platform == "windows":
+        use_mingw = env["use_mingw"]
+        if not use_mingw:
+            print("MSVC target is installed. mvsqlite build skipped.")
+        return False
     return True
 
 
