9170d4c73b4fce2ec39a4cf2c91df75a3f30ef21
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-02-15 20:45:54 -0800

    Try to get mvsc to build.

--- config.py
+++ config.py
@@ -12,22 +12,8 @@ def can_build(env, platform):
     except subprocess.CalledProcessError:
         print("Cargo or Rustup not found. mvsqlite build skipped.")
         if platform == "windows":
-            print("Use `scoop install rustup-gnu` and `rustup target add x86_64-pc-windows-gnu` to install rust.")
+            print("Use `scoop install rustup-gnu` and `rustup target add x86_64-pc-windows-msvc` to install rust.")
         return False
-
-    if platform == "windows":
-        use_mingw = env["use_mingw"]
-        # Check if MSVC target is installed
-        try:
-            targets_output = subprocess.check_output(["rustup", "target", "list", "--installed"], stderr=subprocess.STDOUT)
-            installed_targets = targets_output.decode().splitlines()
-            if "x86_64-pc-windows-msvc" in installed_targets and not use_mingw:
-                print("MSVC target is installed. mvsqlite build skipped.")
-                return False
-        except subprocess.CalledProcessError:
-            print("Failed to list installed rustup targets.")
-            return False
-
     return True
 
 
