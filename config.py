15927be7a3a07d091e0f77741f9bab9ac9bf6700
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-05-06 12:45:53 -0700

    Revert "Build both arm64 and am64 for mvsqlite."
    This reverts commit 80697b4a120c11823756c12fad1449db4bfff049.

--- config.py
+++ config.py
@@ -19,20 +19,18 @@ def can_build(env, platform):
         if not use_mingw:
             print("MSVC target is installed. mvsqlite build skipped.")
         return False
-    
+
     try:
-        installed_targets = subprocess.check_output(["rustup", "target", "list", "--installed"], stderr=subprocess.STDOUT).decode('utf-8')
-
-        if "x86_64-apple-darwin" not in installed_targets:
-            print("x86_64-apple-darwin target not found. Installing...")
-            subprocess.check_call(["rustup", "target", "add", "x86_64-apple-darwin"])
-
-        if "aarch64-apple-darwin" not in installed_targets:
-            print("aarch64-apple-darwin target not found. Installing...")
-            subprocess.check_call(["rustup", "target", "add", "aarch64-apple-darwin"])
-
+        subprocess.check_output(["rustup", "target", "list", "--installed"], stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
-        print("Error occurred while checking installed targets")
+        print("x86_64-apple-darwin target not found. Installing...")
+        subprocess.check_call(["rustup", "target", "add", "x86_64-apple-darwin"])
+
+    try:
+        subprocess.check_output(["rustup", "target", "list", "--installed"], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print("aarch64-apple-darwin target not found. Installing...")
+        subprocess.check_call(["rustup", "target", "add", "aarch64-apple-darwin"])
 
     return True
 
